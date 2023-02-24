0b0e1f8a9335a0ec4e02b48ee1b0ed78bf3cb8e2
bnbkrishna <124280852+bnbkrishna@users.noreply.github.com>
2023-02-23 22:44:56 -0500

    Update example.spec.ts

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,7 +1,8 @@
 import { test, expect } from '@playwright/test';
 
-test('basic test', async ({ page }) => {
+test('basic test', async ({ page },testInfo) => {
   await page.goto('https://playwright.dev/');
   await page.locator('text=Get started').click();
+  testInfo.annotations.push({ type: 'url', description: await page.url() });
   await expect(page).toHaveTitle(/.*Installation/);
 });
